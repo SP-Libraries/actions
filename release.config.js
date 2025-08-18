63fd1ab0b3c369d01c1e91794daa7ee58fe3d42e
Senthilprabu Ponnusamy <p.senthilprabu@gmail.com>
2025-08-17 20:29:37 -0400

    feat: initialize project with configuration files

---
+++ release.config.js
@@ -0,0 +1,35 @@
+export default {
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/SP-Libraries/actions.git',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits'
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits'
+      }
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd:
+          'RELEASE_VERSION=${nextRelease.version} node ./release.update.js'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message:
+          'chore(release): v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/github'
+  ]
+};
